63836cbe641e3ab0c34dbc2e1756a4e3804d6005
nanakiksc <polcusco@gmail.com>
2014-11-20 17:38:44 +0100

    Fixed Tarjan's find() and Vertex() instantiations.

--- tarjan.py
+++ tarjan.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
+# Tarjan's Union-Find algorithm.
+# Find all the connected components (cc) in an undirected graph
+# by means of path compression to the parent vertex of each cc.
+# Parent is arbitrarily chosen among all cc vertices.
+
 import sys
 
 class Vertex():
@@ -14,7 +19,7 @@ def union(vertex1, vertex2):
 
     if root1 < root2:
         root1.parent = root2
-    else:
+    elif root1 > root2:
         root2.parent = root1
     
 def find(vertex):
@@ -23,22 +28,22 @@ def find(vertex):
 
     return vertex
 
+def main(input_file):
+    graph = {}
+    for line in input_file:
+        key1, key2 = [int(x) for x in line.split()]
+
+        if key1 not in graph:
+             graph[key1] = Vertex(key1)
+
+        if key2 not in graph:
+             graph[key2] = Vertex(key2)
+
+        union(graph[key1], graph[key2])
+
+    return graph
+
 
 if __name__ == '__main__':
-    seen = {}
     with open(sys.argv[1]) as fin:
-        for line in fin:
-            items = [int(x) for x in line.split()]
-            items[3] -= items[3] % 10
-
-            key1 = items[0], items[1]
-            if key1 not in seen:
-                 vertex1 = Vertex(key1)
-            seen[key1] = vertex1
-
-            key2 = items[2], items[3]
-            if key2 not in seen:
-                 vertex2 = Vertex(key2)
-            seen[key2] = vertex2
-
-            union(vertex1, vertex2)
+        graph = main(fin)
